1a93fa122b6554db48f257c635e0a7d15d9b8c80
alienatedlabor <104693087+Alienatedlabor@users.noreply.github.com>
2023-05-15 18:25:58 -0400

    complete 2.3

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,11 +1,15 @@
 import Content from './Content';
 import Header from './Header';
+import Total from './Total';
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total
+        total={course.parts.reduce((total, part) => total + part.exercises, 0)}
+      />
     </div>
   );
 };
