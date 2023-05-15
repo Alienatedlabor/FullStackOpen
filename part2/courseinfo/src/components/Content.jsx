bef3faf583ea4782257c1aa8989e2882f5b7e4ce
alienatedlabor <104693087+Alienatedlabor@users.noreply.github.com>
2023-05-15 18:02:13 -0400

    complete 2.1

---
+++ part2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import Part from './Part';
+
+const Content = ({ course }) => {
+  return (
+    <>
+      {course.parts.map((part) => (
+        <Part part={part.name} exercises={part.exercises} key={part.id} />
+      ))}
+    </>
+  );
+};
+export default Content;
