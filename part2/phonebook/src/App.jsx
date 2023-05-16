0aa418e420328c6db7ed9bc3a48de918ccf62dcc
alienatedlabor <104693087+Alienatedlabor@users.noreply.github.com>
2023-05-16 14:47:42 -0400

    complete 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,36 @@
-function App() {
-  return <div></div>;
-}
+import { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(personObject));
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:{' '}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p>{person.name}</p>
+      ))}
+    </div>
+  );
+};
 
 export default App;
