5a41394fbadf1152f8eaf852779272e7b6c17560
alienatedlabor <104693087+Alienatedlabor@users.noreply.github.com>
2023-05-25 14:30:33 -0400

    add filter

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function App() {
-  return <div className="App"></div>;
+  const [searchedCountry, setSearchedCountry] = useState('');
+  const [allCountries, setAllCountries] = useState([]);
+
+  const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/';
+
+  const filteredCountries = allCountries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchedCountry.toLowerCase())
+  );
+
+  const handleChange = (e) => {
+    setSearchedCountry(e.target.value);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}/api/all`)
+      .then((response) => setAllCountries(response.data));
+  }, []);
+
+  return (
+    <div className="App">
+      <div>
+        <label>Find countries:</label>
+        <input value={searchedCountry} onChange={handleChange} type="text" />
+      </div>
+      <div>
+        {searchedCountry &&
+          (filteredCountries.length >= 10
+            ? 'Too many matches, filter further'
+            : filteredCountries.map((country) => (
+                <ul key={country.name.common}>
+                  <li>{country.name.common}</li>
+                </ul>
+              )))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
